ec8d18e6f80abf8775b5ef706a1d6b1dc1294f90
xuyulong1216 <50774452+xuyulong1216@users.noreply.github.com>
2025-10-02 01:45:41 +0800

    Add files via upload

--- dumbserver.py
+++ dumbserver.py
@@ -3,15 +3,17 @@ from urllib.parse import urlparse, parse_qs
 #from mulitiprocessing.managers import Manager,Process
 import time
 import json
+
 class dumb_atmo:
     def __init__(self,port,baud):
         self.port=port
         self.addr=1
         self.baud=baud
-        self.sensors=['wind_speed','wind_power','wind_direction_analog','wind_direction_degree','temperature','humidit','noise','variable','PM10','air_pressure','illuminance','rainfall','compass','sun_radient']
+        self.sensors=['wind_speed','wind_power','wind_direction_analog','wind_direction_degree','humidit','tempreture','noise','variable','PM10','air_pressure','illuminance_full','illuminance_short','rainfall','compass','sun_radient']
         self.addrs=[[500],[501],[502],[503],[504],[505],[506],[507],[508],[509],[510,511],[512],[513],[514],[515]]
+#        print (len(self.sensors))
+#        print(len(self.addrs))
         self.compat={self.sensors[i]:self.addrs[i] for i in range(0,len(self.addrs))}
-			
     def read(self,sensor):
         res=0xba
         for i in range(0, 2*len(self.compat[sensor])):
@@ -21,6 +23,9 @@ class dumb_atmo:
     def read_all(self):
         return { k:self.read(k) for k in self.sensors}
 
+    def is_exist(self):
+        return 1
+
 class dumb_gateway:
     def __init__(self,port,baud):
         self.port=port
@@ -29,7 +34,7 @@ class dumb_gateway:
         self.baud=baud
 		#self.ser=serial.Serial(port,baud,timeout=1)
         self.sensors=[i for i in range(1,0x15)]
-			
+
     def read(self,start_addr,data_length):
         res=0xab
         for i in range(0,data_length):
@@ -38,6 +43,9 @@ class dumb_gateway:
 
     def read_all(self):
         return {"%#04X" %  k:self.read(k,2) for k in self.sensors}
+
+    def is_exist(self):
+        return 1
 
 class dumb_predictor:
     def __init__(self):
@@ -90,17 +98,18 @@ class MyHandler(BaseHTTPRequestHandler):
                     self.wfile.write((json.dumps(data)+ '\n').encode('utf-8'))
             elif len(path_l) == 2:
                 try:
-                    data=atmo_meter[0].read(path_l[-1])
+                    data=atmo_meter[0].is_exist()
                 except AttributeError:
                     self.send_error(404,'Not Found')
                 else:
-                    if int(path_l[-1]) in atmo_meter[0].sensors :
+                    if path_l[-1] in atmo_meter[0].sensors :
                         self.send_response(200)
                         self.send_header('Content-type', 'application/json')
                         self.end_headers()
+                        data=data=atmo_meter[0].read(path_l[-1])
                         self.wfile.write((json.dumps({ path_l[-1] :data})+ '\n').encode('utf-8'))
-                    elifif path_l[-1] == 'list_sensors':
-                     self.send_response(200)
+                    elif path_l[-1] == 'list_sensors':
+                        self.send_response(200)
                         self.send_header('Content-type', 'application/json')
                         self.end_headers()
                         self.wfile.write((json.dumps({'sensors':str(atmo_meter[0].sensors)})+ '\n').encode('utf-8'))
@@ -150,8 +159,8 @@ class MyHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         path_l=self.path.split('/')
         del(path_l[0])
-
-        if self.path not in ['/gateway/setting','/atmosphere/setting']or  'application/json' not in self.headers.get('Content-Type',''):
+        print(self.path)
+        if self.path not in ['/gateway/setting','/atmosphere/setting'] or  'application/json' not in self.headers.get('Content-Type',''):
             self.send_error(400,'Bad Request')
         elif path_l[0] == 'gateway':
             req=eval(self.rfile.read(int(self.headers.get('Content-Length', 0))).decode('utf-8'))
@@ -161,12 +170,12 @@ class MyHandler(BaseHTTPRequestHandler):
         elif path_l[0] == 'atmosphere':
             req=eval(self.rfile.read(int(self.headers.get('Content-Length', 0))).decode('utf-8'))
             
-            atm_meter[0]=dumb_atmo(req['port'],req['baud'])
+            atmo_meter[0]=dumb_atmo(req['port'],req['baud'])
             self.send_error(201,'Created')
 
 if __name__ == '__main__':
     gateway=[1]
-    atm_meter=[1]
+    atmo_meter=[1]
     server_address = ('', 8000)
     predictor=dumb_predictor()
     #manager=Manager()
